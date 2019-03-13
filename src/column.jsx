d59e386a51c3cb9a928a4018f9e7aaad80462ddf
Gabriel Margoses <gabrielmargoses@gmail.com>
2019-03-13 22:13:57 +0100

    added styled-components 💅

--- src/column.jsx
+++ src/column.jsx
@@ -1,7 +1,17 @@
 import React from "react";
+import styled from "styled-components";
+
+const Container = styled.div``;
+const Title = styled.h3``;
+const TaskList = styled.div``;
 
 export default class Column extends React.Component {
   render() {
-    return this.props.column.title;
+    return (
+      <Container>
+        <Title>{this.props.column.title}</Title>
+        <TaskList>Tasks go here</TaskList>
+      </Container>
+    );
   }
 }
