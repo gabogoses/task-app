30944c0e9774b3e7dd7b876c41d55a83b294ed4b
Gabriel Margoses <gabrielmargoses@gmail.com>
2019-03-14 01:12:14 +0100

    appearance customization

--- src/index.js
+++ src/index.js
@@ -8,7 +8,23 @@ import Column from "./column";
 class App extends React.Component {
   state = initialData;
 
+  onDragStart = () => {
+    document.body.style.color = "orange";
+    document.body.style.transition = "background-color 0.2s ease";
+  };
+
+  onDragUpdate = update => {
+    const { destination } = update;
+    const opacity = destination
+      ? destination.index / Object.keys(this.state.tasks).length
+      : 0;
+    document.body.style.backgroundColor = `rgba(153, 141, 217, ${opacity})`;
+  };
+
   onDragEnd = result => {
+    document.body.style.color = "inherit";
+    document.body.style.backgroundColor = "inherit";
+
     const { destination, source, draggableId } = result;
 
     if (!destination) {
@@ -41,11 +57,16 @@ class App extends React.Component {
     };
 
     this.setState(newState);
+    console.log(newState);
   };
 
   render() {
     return (
-      <DragDropContext onDragEnd={this.onDragEnd}>
+      <DragDropContext
+        onDragStart={this.onDragStart}
+        onDragUpdate={this.onDragUpdate}
+        onDragEnd={this.onDragEnd}
+      >
         {this.state.columnOrder.map(columnId => {
           const column = this.state.columns[columnId];
           const tasks = column.taskIds.map(taskId => this.state.tasks[taskId]);
